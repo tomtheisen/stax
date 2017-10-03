f6401f28f3e5372d9d53d03cbfcfac79213bdb7b
Tom Theisen <github@tomtheisen.com>
2017-10-03 08:11:19 -0700

    rational operators

--- StaxLang/Rational.cs
+++ StaxLang/Rational.cs
@@ -26,8 +26,17 @@ namespace StaxLang {
         public static Rational operator +(Rational a, Rational b) => new Rational(a.Num * b.Den - b.Num - a.Den, a.Den * b.Den);
         public static Rational operator /(Rational a, Rational b)=> new Rational(a.Num * b.Den, a.Den * b.Num);
         public static Rational operator *(Rational a, Rational b)=> new Rational(a.Num * b.Num, a.Den * b.Den);
+        public static Rational operator -(Rational a) => a * -1;
+        public static bool operator ==(Rational a, Rational b) => a.Equals(b);
+        public static bool operator !=(Rational a, Rational b) => !a.Equals(b);
+        public static bool operator <(Rational a, Rational b) => a.Num * b.Den < b.Num * a.Den;
+        public static bool operator >(Rational a, Rational b) => a.Num * b.Den > b.Num * a.Den;
+        public static bool operator <=(Rational a, Rational b) => a.Num * b.Den <= b.Num * a.Den;
+        public static bool operator >=(Rational a, Rational b) => a.Num * b.Den >= b.Num * a.Den;
 
-        public static implicit operator Rational(int n) => new Rational(n, 1);
+        public static implicit operator Rational(int n) => n;
+        public static implicit operator Rational(BigInteger n) => new Rational(n, 1);
+
         public override string ToString() => $"{Num}/{Den}";
         public override bool Equals(object obj) => obj is Rational r && Num == r.Num && Den == r.Den;
         public override int GetHashCode() => Num.GetHashCode() ^ (Den.GetHashCode() * 37);
