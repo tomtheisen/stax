988304732fcf2ad3d52004d0989723acbb5f026f
Tom Theisen <github@tomtheisen.com>
2017-11-03 07:59:54 -0700

    runtime

--- StaxLang.JS/stax.ts
+++ StaxLang.JS/stax.ts
@@ -1,4 +1,4 @@
-import {Block, Program} from './block';
+import {Block, Program, parseProgram} from './block';
 
 export class ExecutionState {
     public ip: number;
@@ -12,12 +12,15 @@ export class ExecutionState {
 
 export class Runtime {
     private lineOut: (line: string) => void;
+    private outBuffer = ""; // unterminated line output
 
     constructor(output: (line: string) => void) {
         this.lineOut = output;
     }
 
-    public *runSteps(block: Block): Iterator<ExecutionState> {
+    public *runSteps(block: Block | string): Iterator<ExecutionState> {
+        if (typeof block === "string") block = parseProgram(block);
+
         let ip = 0;
         for (let token of block.tokens) {
             yield new ExecutionState(ip);
