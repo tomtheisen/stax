193a9f252d16e429454b8a55e717e1369582fd78
Tom Theisen <tomtheisen@github.com>
2018-02-03 16:17:27 -0800

    staxjs ui realtime output

--- StaxLang.JS/ui.ts
+++ StaxLang.JS/ui.ts
@@ -1,6 +1,9 @@
 import { Runtime, ExecutionState } from './stax'
 import { setTimeout } from 'timers';
 
+// duration to run stax program before yielding to ui and pumping messages
+const workMilliseconds = 20;
+
 const runButton = document.getElementsByTagName("button")[0];
 const codeArea = document.getElementById("code") as HTMLTextAreaElement;
 const inputArea = document.getElementById("stdin") as HTMLTextAreaElement;
@@ -8,33 +11,47 @@ const statusEl = document.getElementById("status") as HTMLDivElement;
 const outputEl = document.getElementById("output") as HTMLDivElement;
 
 let activeStateIterator: Iterator<ExecutionState> | null = null;
-let steps = 0, start = 0, output: string[] = [];
+let steps = 0, start = 0;
 
 function iterateProgramState() {
     if (!activeStateIterator) return;
 
-    steps += 1;
-    let elapsed = Math.ceil(new Date().valueOf() - start);
+    let result: IteratorResult<ExecutionState>, sliceStart = performance.now();
+    while (!(result = activeStateIterator.next()).done) {
+        steps += 1;
+        if(performance.now() - sliceStart > workMilliseconds) break;
+    }
+    
+    if (!result.done) pendWork(iterateProgramState);
+    
+    let elapsed = Math.ceil(performance.now() - start);
     statusEl.innerText = `${ steps } steps, ${ elapsed }ms`;
-
-    let result = activeStateIterator.next();
-    if (result.done) {
-        outputEl.innerText = output.join("\n");
-    }
-    else {
-        setTimeout(iterateProgramState, 0);
-    }
 }
 
 runButton.addEventListener("click", () => {
-    output = []; 
     steps = 0;
-    start = new Date().valueOf();
+    start = performance.now();
+    outputEl.textContent = "";
 
-    let rt = new Runtime(output.push.bind(output));
+    let rt = new Runtime(line => outputEl.textContent += line + "\n");
     let code = codeArea.value;
     let stdin = inputArea.value.split("\n");
     activeStateIterator = rt.runProgram(code, stdin);
-    
+
     iterateProgramState();
 });
+
+const timeouts: (() => void)[] = [];
+const messageName = "stax-work-pend";
+
+function pendWork(fn: () => void) {
+    timeouts.push(fn);
+    window.postMessage(messageName, "*");
+}
+
+window.addEventListener("message", event => {
+    if (event.source === window && event.data === messageName) {
+        event.stopPropagation();
+        if (timeouts.length) timeouts.shift()!();
+    }
+}, true);
