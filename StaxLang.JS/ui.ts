b4a14e17b557faa51e355a1d7ea5218b2a327e0e
Tom Theisen <tomtheisen@github.com>
2018-02-14 23:10:33 -0800

    embed build info

--- StaxLang.JS/ui.ts
+++ StaxLang.JS/ui.ts
@@ -4,6 +4,10 @@ import { compress } from './huffmancompression';
 import { isPacked, unpack, pack } from './packer';
 import 'url-search-params-polyfill';
 
+declare var __COMMIT_HASH__: string;
+declare var __BUILD_DATE__: string;
+document.getElementById("buildInfo")!.textContent = `${__COMMIT_HASH__} built ${__BUILD_DATE__}`;
+
 // duration to run stax program before yielding to ui and pumping messages
 const workMilliseconds = 20;
 
--- StaxLang.JS/webpack.config.js
+++ StaxLang.JS/webpack.config.js
@@ -1,5 +1,10 @@
 const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
+
+const commitHash = require('child_process')
+  .execSync('git rev-parse --short HEAD')
+  .toString();
 
 module.exports = {
     entry: './js/ui.js',
@@ -8,6 +13,10 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
+        new webpack.DefinePlugin({
+            __COMMIT_HASH__: JSON.stringify(commitHash),
+            __BUILD_DATE__: JSON.stringify(new Date),
+        }),
         new UglifyJSPlugin
     ]
 };
