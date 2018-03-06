115f6d5e81ef0821645069b65cc46454fdf9099f
Tom Theisen <tomtheisen@github.com>
2018-03-05 21:01:29 -0800

    repo link

--- StaxLang/Executor.cs
+++ StaxLang/Executor.cs
@@ -14,7 +14,7 @@ using System.Text.RegularExpressions;
 
 namespace StaxLang {
     public class Executor {
-        public const string VersionInfo = "Stax 1.0.5 - Tom Theisen - https://github.com/ttheisen/stax";
+        public const string VersionInfo = "Stax 1.0.5 - Tom Theisen - https://github.com/tomtheisen/stax";
 
         private bool OutputWritten = false;
         public TextWriter Output { get; private set; }
