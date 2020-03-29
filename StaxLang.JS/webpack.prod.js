5ae497ad245bd66bbe59ab6f163d66804bdac1b2
Tom Theisen <fork@tomtheisen.com>
2020-03-29 10:23:05 -0700

    dependency updates uglify -> terser

--- StaxLang.JS/webpack.prod.js
+++ StaxLang.JS/webpack.prod.js
@@ -1,14 +1,10 @@
 const common = require('./webpack.common');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     ...common,
     optimization: {
         minimize: true,
-        minimizer: [new UglifyJsPlugin({
-             uglifyOptions:{
-                 safari10: true
-             }
-        })], 
+        minimizer: [new TerserPlugin()],
     }
 };
