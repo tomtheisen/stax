4678370ea8a6ec3e615f187d486d9cbc46d59191
Tom Theisen <github@tomtheisen.com>
2020-12-12 09:48:28 -0800

    version bump

--- StaxLang.JS/types.ts
+++ StaxLang.JS/types.ts
@@ -220,7 +220,7 @@ export function literalFor(arg: StaxInt): string {
     return arg.toString();
 }
 
-const versionInfo = "Stax 1.1.10 - Tom Theisen - https://github.com/tomtheisen/stax"
+const versionInfo = "Stax 1.1.11 - Tom Theisen - https://github.com/tomtheisen/stax"
 
 export const constants: {[key: string]: StaxValue} = {
     '?': S2A(versionInfo),
